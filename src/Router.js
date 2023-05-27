e1aa004debaec730d1cd742ac7b4f61bd4c499fb
Mukul <mukul.lakh@gmail.com>
2023-05-27 20:19:46 +0530

    Removed FileDownload

--- src/Router.js
+++ src/Router.js
@@ -6,7 +6,6 @@ import Contact from './components/Contact';
 import { BrowserRouter } from 'react-router-dom';
 import { initGA, trackPageView } from './analytics';
 import { createBrowserHistory } from 'history';
-import FileDownload from './FileDownload';
 
 
 function Router() {
@@ -21,7 +20,6 @@ function Router() {
         <Route index path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
-        <Route path=".well-known/pki-validation/38F68A5F3E44232B71EC5D35EE90F238.txt" element={<FileDownload />} />
       </Routes>
     </BrowserRouter>
 
